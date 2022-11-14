55522a8050151c947b83e34570eec369d1dbb6ff
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2022-11-14 10:21:49 +0700

    Update java scaner.java

--- java scaner.java
+++ java scaner.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
-
-public class Main {
+class Main {
   public static void main(String[] args) {
     Scanner Keyboard = new Scanner(System.in);
+    int d,e,f,c,year;
+    int a = 5;
+    int b = 10;
+    
+   
+    System.out.print("number d :");
+    d = Keyboard.nextInt();
+    System.out.print("number e :");
+    e = Keyboard.nextInt();
 
+    
     System.out.print("What's your name :");
     String name = Keyboard.nextLine();
+    
     System.out.print("what's your surname :");
     String sname = Keyboard.nextLine();
 
-    System.out.print("How old are your :");
+    System.out.print("What is your birth year 25__?:");
     int olds = Keyboard.nextInt();
 
+    year = 2565 - olds;
+
+    
+    
+    c = a + b;
+    f = d + e;
+    System.out.println("a");
+    System.out.println(c);
+   System.out.println(f);
     System.out.println(name + "  " + sname);
    
-    System.out.println(olds);
-
+    System.out.println(year);
+    
+    
   }
 }
